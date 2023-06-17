211130db4247e29b38ad8f7472b9479404bda57f
Michal <salatman13@gmail.com>
2023-06-17 17:32:00 +0200

    0.0.1 Commiting project

---
+++ src/main/java/com/kodilla/KodillaMichalLisekFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaMichalLisekFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaMichalLisekFirstProjectApplication.class, args);
+	}
+
+}
